9ecbdae1b2cb8952749864b611e2a2316aed5dc0
irobertcostin <irobertcostin@gmail.com>
2024-05-08 10:28:18 +0300

    created module,service and controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,37 @@
-import { Controller } from '@nestjs/common';
+import { Controller, UseInterceptors } from '@nestjs/common';
+import { EmailInterceptor } from 'src/interceptors/email.interceptor';
+import { IdValidationInterceptor } from 'src/interceptors/id-validation.interceptor';
+import { UsersService } from './users.service';
+import { Post, Body } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { LoginUserDto } from './dto/login.dto';
+import { UsernameInterceptor } from 'src/interceptors/username.interceptor';
+
 
 @Controller('users')
-export class UsersController {}
+@UseInterceptors(EmailInterceptor, IdValidationInterceptor, UsernameInterceptor)
+export class UsersController {
+
+    constructor(private userService: UsersService) { }
+
+    @Post('register')
+    register(
+        @Body()
+        createUserDto: CreateUserDto
+    ): Promise<{ token: string }> {
+        return this.userService.register(createUserDto)
+    }
+
+
+
+    @Post('login')
+    login(
+        @Body()
+        loginUserDto: LoginUserDto
+    ): Promise<{ token: string }> {
+        return this.userService.login(loginUserDto)
+    }
+
+
+
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,76 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable, NotFoundException, UnauthorizedException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import * as mongoose from "mongoose"
+import { JwtService } from "@nestjs/jwt"
+import * as bcrypt from "bcryptjs"
+import { CreateUserDto } from './dto/create-user.dto';
+import { LoginUserDto } from './dto/login.dto';
+import { GetUserDto } from './dto/getuser-dto';
+import { User } from './schema/users.schema';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+
+    constructor(
+        @InjectModel(User.name)
+        private userModel: mongoose.Model<User>,
+        private jwtService: JwtService
+    ) { }
+
+
+    async register(createUserDto: CreateUserDto): Promise<{ token: string }> {
+
+        const { gender, birthday, username, email, password } = createUserDto;
+
+        const user = await this.userModel.findOne({
+            $or: [{ email }, { username }]
+        });
+
+        if (user) {
+            throw new ConflictException('This user is already registered')
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const newUser = await this.userModel.create({
+            gender,
+            birthday,
+            username,
+            email,
+            password: hashedPassword,
+        })
+
+        const token = this.jwtService.sign({ id: newUser._id })
+
+        return { token }
+    }
+
+
+    async login(loginDto: LoginUserDto): Promise<{ token: string }> {
+
+        const { email, username, password } = loginDto;
+
+
+        const user = await this.userModel.findOne({
+            $or: [{ email }, { username }]
+        });
+
+        if (!user) {
+            throw new NotFoundException('Invalid credentials')
+        }
+
+        const isPasswordMatched = await bcrypt.compare(password, user.password)
+
+        if (!isPasswordMatched) {
+            throw new UnauthorizedException('Invalid password')
+        }
+
+        const token = this.jwtService.sign({ id: user._id })
+
+        return { token }
+
+    }
+
+
+
+}
